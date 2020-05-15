8ab68ff72deda4496d0781f7d1561ecf6ff761e6
KerimHelal <kareemelsayed1997@gmail.com>
2020-05-15 23:18:20 +0200

    initial

---
+++ node_modules/gh-pages/.cache/github.com!KerimHelal!foody-test.git/precache-manifest.bbe3eec7a7361152001d209ad1856641.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5605cf7351bec9505d5681655fd59b64",
+    "url": "/foody-test/index.html"
+  },
+  {
+    "revision": "0f36318d3e1cedccd1d0",
+    "url": "/foody-test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b86e8440aaf10275f11d",
+    "url": "/foody-test/static/js/2.348b1b41.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/foody-test/static/js/2.348b1b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f36318d3e1cedccd1d0",
+    "url": "/foody-test/static/js/main.7134fbf0.chunk.js"
+  },
+  {
+    "revision": "0871bee53a37b216f039",
+    "url": "/foody-test/static/js/runtime-main.36506aec.js"
+  }
+]);
